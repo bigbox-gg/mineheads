18ea3396463e9204f0915df6f82c86f086d9147f
Nicolas Vaccari <vcc.nicolas@gmail.com>
2022-01-14 14:40:02 +0100

    - Add gradle compilation - Use project.version in extension.json

--- minestom/build.gradle.kts
+++ minestom/build.gradle.kts
@@ -1,3 +1,30 @@
+plugins {
+    java
+    idea
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+}
+
 dependencies {
     implementation(project(":api"))
+}
+
+tasks {
+    processResources {
+        // Apply properties to extension.json
+        filesMatching("extension.json") {
+            expand(project.properties)
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        archiveBaseName.set("mineheads")
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "gg.bigbox.minecraft.plugins.mineheads.minestom.MineHeadsMinestomImpl"))
+        }
+    }
 }
